5e5e1e46a1ba2e324425d904f69c9e5b54305d28
MatLukac <mat.lukac13@gmail.com>
2025-10-15 23:57:49 +0200

    Update App.js

--- the-way/src/App.js
+++ the-way/src/App.js
@@ -1,4 +1,4 @@
-// App.jsx (alebo iná hl. komponenta)
+
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import TeamSection from "./components/TeamSection";
@@ -7,26 +7,28 @@ import Footer from "./components/Footer";
 import Newsletter from "./components/Newsletter";
 import NewsGrid from "./components/NewsGrid";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 
 
 function App() {
   return (
     <div>
+      <BrowserRouter basename="/twccg" >
       <Header />
       
       <main className="pt-16">  {/* offset kvôli fixnému headeru */}
         {/* zvyšok obsahu */}
 
-        <Banner />
-        <InfoSection />
-        <NewsGrid />
-        <Newsletter />
-
-        <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
         
       </main>
-      
+      </BrowserRouter>
     </div>
   );
 }
